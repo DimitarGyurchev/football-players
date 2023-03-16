a2982854021434b91f6acb5714bd16a8e4011281
Dimitar Gyurchev <dimitargyurchev93@gmail.com>
2023-03-16 17:28:55 +0100

    Home component modified

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,7 +5,7 @@ export const Home = () => {
     return (
         <div className={styles.containerHome}>
             <h1 className={styles.title}>Football Players</h1>
-            <h2 className={styles.subtitle}>Find your favorite players and create your own team!</h2>
+            <h2 className={styles.subtitle}>Create your favorite players and your own team!</h2>
         </div>
     );
 }
