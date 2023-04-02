f8dc84bc1a8ce4b0ec45b4f294ad207f949c96de
Dimitar Gyurchev <dimitargyurchev93@gmail.com>
2023-04-02 17:11:19 +0200

    header auth fixed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,22 +1,36 @@
 import styles from "./Header.module.css"
+
+import { useContext } from 'react';
 import { Link } from 'react-router-dom'
+
+import { AuthContext } from '../../contexts/AuthContext';
+
 export const Header = () => {
+
+    const { isAuthenticated, userEmail } = useContext(AuthContext);
+
     return (
 
         <header>
             <nav>
                 <div>
                     <Link to="/">Home</Link>
-                </div>
-                <div id="user">
                     <Link to="/catalog">All Football Players</Link>
-                    <Link to="/create">Create Football Player</Link>
-                    <Link to="/logout">Logout</Link>
                 </div>
-                <div id="guest">
-                    <Link to="/register">Register</Link>
-                    <Link to="/login">Login</Link>
-                </div>
+                {isAuthenticated && (
+                    <div id="user">
+                        <span id="userEmail">Welcome: {userEmail}</span>
+                        <Link to="/create">Create Football Player</Link>
+                        <Link to="/logout">Logout</Link>
+                    </div>
+                )}
+
+                {!isAuthenticated && (
+                    <div id="guest">
+                        <Link to="/register">Register</Link>
+                        <Link to="/login">Login</Link>
+                    </div>
+                )}
             </nav>
         </header>
     );
