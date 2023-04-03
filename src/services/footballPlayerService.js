6962432b1d04fccd13f25b92c1b3a6a5a8b7c78d
Dimitar Gyurchev <dimitargyurchev93@gmail.com>
2023-04-03 10:37:28 +0200

    football Service added

---
+++ src/services/footballPlayerService.js
@@ -0,0 +1,39 @@
+import { requestFactory } from './requester';
+
+const baseUrl = 'http://localhost:3030'
+const url = `${baseUrl}/data/games`;
+
+export const footballPlayerServiceFactory = (token) => {
+    const request = requestFactory(token);
+
+    const getAll = async () => {
+        const result = await request.get(url);
+        const players = Object.values(result);
+
+        return players;
+    }
+
+    const getOne = async (playerId) => {
+        const result = await request.get(`${url}/${gameId}`);
+
+        return result;
+    }
+
+    const create = async (playerData) => {
+        const result = await request.post(url, playerData);
+
+        return result;
+    }
+
+    const edit = (playerId, data) => request.put(`${url}/${playerId}`, data);
+
+    const deletePlayer = (playerId) => request.delete(`${url}/${playerId}`);
+
+    return {
+        getAll,
+        getOne,
+        create,
+        edit,
+        delete: deletePlayer
+    }
+}
